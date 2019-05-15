47be8e975852a5f86dee27df23024a40645f7c8d
Deven Fafard <deven.marie.fafard@gmail.com>
2019-05-15 04:45:36 -0700

    Completed task 5

--- five.py
+++ five.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python
 
+import sys
 
+args = sys.argv[1:]
+keywords = sys.argv[2:]
+
+with open(args[0], 'r+') as log_file:
+	lines = log_file.readlines()
+	log_file.seek(0)
+
+	for line in lines:
+		for word in keywords:
+			if (word in line):
+				# Keyword match, don't write back to file
+				break
+			else:
+				log_file.write(line)
+				
+	log_file.truncate()
