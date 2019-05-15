1de7aa0f8dfa3077b153d8827561afb21b80f41a
Deven Fafard <deven.marie.fafard@gmail.com>
2019-05-15 08:47:38 -0700

    ACTUALLY completed task 5

--- five.py
+++ five.py
@@ -7,14 +7,16 @@ keywords = sys.argv[2:]
 
 with open(args[0], 'r+') as log_file:
 	lines = log_file.readlines()
-	log_file.seek(0)
+	log_file.seek(0)	
+	log_file.truncate()
 
 	for line in lines:
+		found = False
 		for word in keywords:
 			if (word in line):
 				# Keyword match, don't write back to file
+				found = True
 				break
-			else:
-				log_file.write(line)
-				
-	log_file.truncate()
+		if (not found):
+			print("Line should be in file")
+			log_file.write(line)
