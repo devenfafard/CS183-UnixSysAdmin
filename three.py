6eee324da36cb45ef88b3021ff1e5048090b5286
Deven Fafard <deven.marie.fafard@gmail.com>
2019-05-18 17:39:34 -0700

    Corrected output for task 3

--- three.py
+++ three.py
@@ -25,17 +25,19 @@ for arg in args:
 			split_lines = line.split("from=<")
 			fully_split = split_lines[1].split(">")
 
-			if(fully_split[0] in from_dict):
-				from_dict[fully_split[0]] += 1
+			if fully_split[0] == "":
+				pass
+
 			else:
-				from_dict[fully_split[0]] = 1
+				if(fully_split[0] in from_dict):
+					from_dict[fully_split[0]] += 1
+				else:
+					from_dict[fully_split[0]] = 1
 		
-	print("****** Top 5 To Addresses *****")	
 	sorted_to = sorted(to_dict.items(), key=lambda item: item[1], reverse=True)
 	for x in list(sorted_to)[0:5]:
-		print(x)
+		print("To " + str(x[0]) + " " + str(x[1]))
 
-	print("***** Top 5 From Addresses ******")
 	sorted_from = sorted(from_dict.items(), key=lambda item: item[1], reverse=True)
-	for x in list(sorted_from)[1:6]:
-		print(x)
+	for x in list(sorted_from)[0:5]:
+		print("From " + str(x[0]) + " " + str(x[1]))
